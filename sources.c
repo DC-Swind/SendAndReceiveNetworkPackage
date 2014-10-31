24216a54059e065e939221a27bdc05a3d02c3c96
Swind <dc.swind@gmail.com>
2014-10-31 19:09:51 +0800

    Update sources.c

--- sources.c
+++ sources.c
@@ -7,6 +7,7 @@
 #include <linux/in.h>
 #include <linux/ip.h>
 #include <linux/icmp.h>
+
 #define BUFFER_MAX 2048
 
 int init_send(){
@@ -57,10 +58,7 @@ int init_send(){
     }
 }
 
-int main(int argc,char* argv[]){
-    //before receive, send a icmp package
-    init_send();
-    //------------------------------
+int start_receive(){
     int sock_fd;
     int proto;
     int n_read;
@@ -85,11 +83,39 @@ int main(int argc,char* argv[]){
         p = eth_head;
         printf("-------------------Start Of A Package---------------------\n");
         printf("MAC address: %.2x:%02x:%02x:%02x:%02x:%02x==> %.2x:%02x:%02x:%02x:%02x:%02x\n",p[6],p[7],p[8],p[9],p[10],p[11],p[0],p[1],p[2],p[3],p[4],p[5]);
-        //减小时间复杂度，使用GOTO语句
+        //加快运行速度，使用GOTO语句
         switch(p[12]<<8+p[13]){
+            case 0x0600: printf("XEROX NS IDP \n"); goto End; break;
+            case 0x0660:case 0x0661 printf("DLOG \n"); goto End; break;
             case 0x0800: printf("IP Header: \n"); goto Net_IP; break;
-            case 0x8035: printf("RARP: \n"); goto Net_RARP; break;
-            default: ;
+            case 0x0801: printf("X.75 Internet \n"); goto End; break;
+            case 0x0802: printf("NBS Internet \n"); goto End; break;
+            case 0x0803: printf("ECMA Internet \n"); goto End; break;
+            case 0x0804: printf("Chaosnet \n"); goto End; break;
+            case 0x0805: printf("X.25 Level 3 \n"); goto End; break;
+            case 0x0806: printf("ARP- Address Resolution Protocol \n"); goto End; break;
+            case 0x0808: printf("ARP- Frame Relay ARP[RFC1701] \n"); goto End; break;
+            case 0x8035: printf("RARP \n"); goto Net_RARP; break;
+            case 0x8037: printf("Novell Netware IPX \n"); goto Net_RARP; break;
+            case 0x809B: printf("EtherTalk \n"); goto Net_RARP; break;
+            case 0x80D5: printf("IBM SNA Services over Ethernet \n"); goto Net_RARP; break;
+            case 0x80F3: printf("AppleTalk Address Resolution Protocol \n"); goto Net_RARP; break;
+            case 0x8100: printf("Ethernet Automatic Protection Switching \n"); goto Net_RARP; break;
+            case 0x8137: printf("Internet Packet Exchange \n"); goto Net_RARP; break;
+            case 0x814C: printf("Simple Network Management Protocol \n"); goto Net_RARP; break;
+            case 0x86DD: printf("Internet Protocol version 6 \n"); goto Net_RARP; break;
+            case 0x880B: printf("Point-to-Point Protocol \n"); goto Net_RARP; break;
+            case 0x880C: printf("General Switch Management Protocol \n"); goto Net_RARP; break;
+            case 0x8847: printf("Multi-Protocol Label Switching <unicast> \n"); goto Net_RARP; break;
+            case 0x8848: printf("Multi-Protocol Label Switching <multicast> \n"); goto Net_RARP; break;
+            case 0x8863: printf("PPP Over Ethernet <Discovery Stage> \n"); goto Net_RARP; break;
+            case 0x8864: printf("PPP Over Ethernet<PPP Session Stage> \n"); goto Net_RARP; break;
+            case 0x88BB: printf("Light Weight Access Point Protocol \n"); goto Net_RARP; break;
+            case 0x88CC: printf("Link Layer Discovery Protocol \n"); goto Net_RARP; break;
+            case 0x8E88: printf("EAP over LAN \n"); goto Net_RARP; break;
+            case 0x9000: printf("Loopback \n"); goto Net_RARP; break;
+            case 0x9100: case 0x9200: printf("VLAN Tag Protocol Identifier \n"); goto Net_RARP; break;
+            default: printf("do not support\n");;
         }
         
     Net_IP:;
@@ -103,7 +129,9 @@ int main(int argc,char* argv[]){
         struct icmphdr* icmp = ip_head +20;
         printf("Protocol:");
         switch(proto){
-            case IPPROTO_ICMP:printf("icmp\n");break;
+            case IPPROTO_ICMP:printf("icmp\n");
+                
+                break;
             case IPPROTO_IGMP:printf("igmp\n");break;
             case IPPROTO_IPIP:printf("ipip\n");break;
             case IPPROTO_TCP: printf("tcp\n"); break;
@@ -116,5 +144,12 @@ int main(int argc,char* argv[]){
         
     End:;
     }
+    return -1;
+}
+int main(int argc,char* argv[]){
+    //before receive, send a icmp package
+    init_send();
+    //------------------------------
+    start_receive();
     return -1;
 }
